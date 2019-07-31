d0e5cd9965cb6f5b860a686f1184fd0aaec7bfa9
Dmytro Ostapchuk <dmytro.ostapchuk@ua.pt>
2019-07-31 10:31:55 +0100

    modelWH.py

--- prepareData.py
+++ prepareData.py
@@ -21,13 +21,7 @@ np.random.seed(seed)
 otherFeatures = ['PUWeight','flavB1', 'flavB2', 'EventNumber', 'EventRegime', 'AverageMu', 'EventWeight', 'Sample', 'Description', 'EventFlavor', 'TriggerSF', 'ActualMuScaled', 'AverageMuScaled', 'eventFlagMerged/l','eventFlagResolved/l','BTagSF','ActualMu','LeptonSF']
 trainFeatures = ['nFats', 'nJets', 'nTags', 'nTaus', 'nMuons', 'nbJets', 'FJ1nTags', 'nFwdJets', 'nSigJets', 'nElectrons', 'mB1', 'mB2', 'mBB', 'mJ3', 'mL1', 'mTW', 'mVH', 'met', 'pTW', 'FJ1M', 'dRBB', 'mBBJ', 'mVFJ', 'pTB1', 'pTB2', 'pTBB', 'pTJ3', 'phiW', 'ptL1', 'FJ1C2', 'FJ1D2', 'FJ1Pt', 'etaB1', 'etaB2', 'etaBB', 'etaJ3', 'etaL1', 'pTBBJ', 'phiB1', 'phiB2', 'phiBB', 'phiJ3', 'phiL1', 'FJ1Ang', 'FJ1Eta', 'FJ1Phi', 'FJ1T21', 'dEtaBB', 'dPhiBB', 'metSig', 'FJ1KtDR', 'dPhiVBB', 'dPhiVFJ', 'MV2c10B1', 'MV2c10B2', 'metSig_PU', 'mindPhilepB', 'metOverSqrtHT', 'metOverSqrtSumET']
 
-nrows_signal = 991141
-nrows_stopWt = 277816
-nrows_ttbar = 4168037
-nrows_Wjets = 16650877
-nrows_WlvZqq = 188395
-nrows_WqqWlv = 334495
-
+'''
 start = time.time()
 print("Reading -> 'qqWlvHbbJ_PwPy8MINLO_ade.csv'")
 df_signal = pd.read_csv('data/qqWlvHbbJ_PwPy8MINLO_ade.csv')
@@ -47,28 +41,36 @@ df_WqqWlv = pd.read_csv('data/WqqWlv_Sh221_ade.csv')
 print("Reading -> 'WJets_Sh221.csv'")
 df_WJets = pd.read_csv('data/WJets_Sh221.csv',nrows=int(nrows_Wjets/50))
 print "Reading time: ", (time.time() - start)
-
 '''
+
+nrows_signal = 991141
+nrows_stopWt = 277816
+nrows_ttbar = 4168037
+nrows_Wjets = 16650877
+nrows_WlvZqq = 188395
+nrows_WqqWlv = 334495
+
+i = 0.1
 start = time.time()
 print("Reading -> 'qqWlvHbbJ_PwPy8MINLO_ade.csv'")
-df_signal = pd.read_csv('data/qqWlvHbbJ_PwPy8MINLO_ade.csv',nrows = 1000)
+df_signal = pd.read_csv('data/qqWlvHbbJ_PwPy8MINLO_ade.csv',nrows = int(nrows_signal*i))
 
 print("Reading -> 'stopWt_PwPy8_ade.csv'")
-df_stopWt = pd.read_csv('data/stopWt_PwPy8_ade.csv',nrows = 1000)
+df_stopWt = pd.read_csv('data/stopWt_PwPy8_ade.csv',nrows = int(nrows_stopWt*i))
 
 print("Reading -> 'ttbar_nonallhad_PwPy8_ade.csv'")
-df_ttbar = pd.read_csv('data/ttbar_nonallhad_PwPy8_ade.csv',nrows = 1000)
+df_ttbar = pd.read_csv('data/ttbar_nonallhad_PwPy8_ade.csv',nrows = int(nrows_ttbar*i))
 
 print("Reading -> 'WlvZqq_Sh221_ade.csv'")
-df_WlvZqq = pd.read_csv('data/WlvZqq_Sh221_ade.csv',nrows = 1000)
+df_WlvZqq = pd.read_csv('data/WlvZqq_Sh221_ade.csv',nrows = int(nrows_WlvZqq*i))
 
 print("Reading -> 'WqqWlv_Sh221_ade.csv'")
-df_WqqWlv = pd.read_csv('data/WqqWlv_Sh221_ade.csv',nrows = 1000)
+df_WqqWlv = pd.read_csv('data/WqqWlv_Sh221_ade.csv',nrows = int(nrows_WqqWlv*i))
 
 print("Reading -> 'WJets_Sh221.csv'")
-df_WJets = pd.read_csv('data/WJets_Sh221.csv',nrows = 1000)
+df_WJets = pd.read_csv('data/WJets_Sh221.csv',nrows = int(nrows_Wjets/40))
 print "Reading time: ", (time.time() - start)
-'''
+
 df_signal["category"] = 1
 df_stopWt["category"] = 0
 df_ttbar["category"] = 0
