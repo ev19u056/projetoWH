bf1caeaed9aa85fd83c93b2171be6d20097997e6
Dmytro Ostapchuk <dmytro.ostapchuk@ua.pt>
2019-07-31 10:22:49 +0100

    modelWH.py

--- trainNN.sh
+++ trainNN.sh
@@ -1,7 +1,19 @@
-cd /home/t3atlas/ev19u056/projetoWH
+#!/bin/bash
+#$ -pe mcore 3
 
-module load root
-python modelWH1.py -e 100 -a 5 -b 10e-3 -l 8 -v -i 2
+#$ -l container=True
+#$ -v CONTAINER=UBUNTU16
+
+#$ -v SGEIN=modelWH.py
+#$ -v SGEIN=prepareData.py
+#$ -v SGEIN=localConfig.py
+#$ -v SGEIN=commonFunctions.py
+
+#$ -v SGEOUT=test:/home/t3atlas/ev19u056/projetoWH
+
+#cd /home/t3atlas/ev19u056/projetoWH
+
+python modelWH.py -e 100 -a 3000 -b 0.01 -l "64 62 62 62" -v -i 2
 git add .
-git commit -m "iris_example"
+git commit -m "modelWH.py"
 git push
