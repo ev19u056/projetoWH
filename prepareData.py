da5baa696c35a3ae5bc47b274c68a7fc3306caa2
Dmytro Ostapchuk <dmytro.ostapchuk@ua.pt>
2019-08-01 16:14:48 +0100

    preoareData.py

--- prepareData.py
+++ prepareData.py
@@ -45,9 +45,9 @@ start = time.time()
 print("Reading -> 'qqWlvHbbJ_PwPy8MINLO_ade.csv'")
 tmp = pd.read_csv('data/qqWlvHbbJ_PwPy8MINLO_ade.csv',chunksize=chunksize,nrows = int(nrows_signal*fraction))
 df_signal = chunkReader(tmp)
-print(df.EventWeight[0])
+print(df_signal.EventWeight[0])
 df_signal[["EventWeight"]] = df_signal[["EventWeight"]]*fraction
-print(df.EventWeight[0])
+print(df_signal.EventWeight[0])
 
 print("Reading -> 'stopWt_PwPy8_ade.csv'")
 tmp = pd.read_csv('data/stopWt_PwPy8_ade.csv',chunksize=chunksize,nrows = int(nrows_stopWt*fraction))
