f0eca57d6fddb3eef8e918ce7e3ad69196d2e6db
Dmytro Ostapchuk <dmytro.ostapchuk@ua.pt>
2019-08-01 17:53:11 +0100

    preoareData.py

--- prepareData.py
+++ prepareData.py
@@ -105,11 +105,11 @@ XVal = data[trainFeatures].ix[Dev_len:,:]
 YVal = data[["category"]].ix[Dev_len:,:]
 
 
-weightDev = data[["EventWeight"]].ix[0:Dev_len-1,:]
-weightDev = weightDev[["EventWeight"]].to_numpy()
+weightDev1 = data[["EventWeight"]].ix[0:Dev_len-1,:]
+weightDev = weightDev1["EventWeight"].tolist()
 print(weightDev)
-weightVal = data[["EventWeight"]].ix[Dev_len:,:]
-weightVal = weightVal.to_numpy()
+#weightVal1 = data[["EventWeight"]].ix[Dev_len:,:]
+#weightVal = weightVal.to_numpy()
 
 del data
 print 'XDev: ', len(XDev), ' YDev: ', len(YDev), ' weightDev: ', len(weightDev)
