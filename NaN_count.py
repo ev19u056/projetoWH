fb9b5f6152f386b72531e997c26db1f8105287e8
Dmytro Ostapchuk <dmytro.ostapchuk@ua.pt>
2019-08-02 10:14:59 +0100

    NaNcount.py

--- NaN_count.py
+++ NaN_count.py
@@ -14,21 +14,32 @@ nrows_Wjets = 16650877
 nrows_WlvZqq = 188395
 nrows_WqqWlv = 334495
 
+def missing_values_table(df):
+        mis_val = df.isnull().sum()
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        mis_val_table_ren_columns = mis_val_table.rename(columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        mis_val_table_ren_columns = mis_val_table_ren_columns[mis_val_table_ren_columns.iloc[:,1] != 0].sort_values('% of Total Values', ascending=False).round(1)
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        return mis_val_table_ren_columns
+
 fraction = 0.1
 start = time.time()
 print("Reading -> 'qqWlvHbbJ_PwPy8MINLO_ade.csv'")
 tmp = pd.read_csv('data/qqWlvHbbJ_PwPy8MINLO_ade.csv',nrows = int(nrows_signal*fraction))
-nan_count = tmp[trainFeatures].isna().sum()
+#nan_count = tmp[trainFeatures].isna().sum(axis = 0)
+missing_values_table(tmp)
 del tmp
-
-
-print("Reading -> 'stopWt_PwPy8_ade.csv'")
-tmp = pd.read_csv('data/stopWt_PwPy8_ade.csv',nrows = int(nrows_stopWt*fraction))
-nan_count = nan_count.append(tmp[trainFeatures].isna().sum())
-del tmp
-print(nan_count.dtype)
 
 '''
+print("Reading -> 'stopWt_PwPy8_ade.csv'")
+tmp = pd.read_csv('data/stopWt_PwPy8_ade.csv',nrows = int(nrows_stopWt*fraction))
+nan_count = nan_count.append(tmp[trainFeatures].isna().sum(axis = 0))
+del tmp
+print(nan_count)
+
 print("Reading -> 'ttbar_nonallhad_PwPy8_ade.csv'")
 tmp = pd.read_csv('data/ttbar_nonallhad_PwPy8_ade.csv',chunksize=chunksize,nrows = int(nrows_ttbar*fraction))
 del tmp
