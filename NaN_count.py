ae51a99762d4ccf00435847f836a39838be1271a
Dmytro Ostapchuk <dmytro.ostapchuk@ua.pt>
2019-08-02 10:52:37 +0100

    NaNcount.py

--- NaN_count.py
+++ NaN_count.py
@@ -21,44 +21,41 @@ def missing_values_table(df):
         mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
         mis_val_table_ren_columns = mis_val_table.rename(columns = {0 : 'Missing Values', 1 : '% of Total Values'})
         mis_val_table_ren_columns = mis_val_table_ren_columns[mis_val_table_ren_columns.iloc[:,1] != 0].sort_values('% of Total Values', ascending=False).round(2)
-        #print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n")
         #    "There are " + str(mis_val_table_ren_columns.shape[0]) +
         #      " columns that have missing values.")
         return mis_val_table_ren_columns
 
-fraction = 0.1
+fraction = 1
 start = time.time()
 print("Reading -> 'qqWlvHbbJ_PwPy8MINLO_ade.csv'")
 tmp = pd.read_csv('data/qqWlvHbbJ_PwPy8MINLO_ade.csv',nrows = int(nrows_signal*fraction),usecols=trainFeatures,skipinitialspace=True)
-print tmp.keys()
-'''
 print(missing_values_table(tmp[trainFeatures]))
 del tmp
 
 print("Reading -> 'stopWt_PwPy8_ade.csv'")
-tmp = pd.read_csv('data/stopWt_PwPy8_ade.csv',nrows = int(nrows_stopWt*fraction))
+tmp = pd.read_csv('data/stopWt_PwPy8_ade.csv',nrows = int(nrows_stopWt*fraction),usecols=trainFeatures,skipinitialspace=True)
 print(missing_values_table(tmp[trainFeatures]))
 del tmp
 
 
 print("Reading -> 'ttbar_nonallhad_PwPy8_ade.csv'")
-tmp = pd.read_csv('data/ttbar_nonallhad_PwPy8_ade.csv',nrows = int(nrows_ttbar*fraction))
+tmp = pd.read_csv('data/ttbar_nonallhad_PwPy8_ade.csv',nrows = int(nrows_ttbar*fraction),usecols=trainFeatures,skipinitialspace=True)
 print(missing_values_table(tmp[trainFeatures]))
 del tmp
 
 print("Reading -> 'WlvZqq_Sh221_ade.csv'")
-tmp = pd.read_csv('data/WlvZqq_Sh221_ade.csv',nrows = int(nrows_WlvZqq*fraction))
+tmp = pd.read_csv('data/WlvZqq_Sh221_ade.csv',nrows = int(nrows_WlvZqq*fraction),usecols=trainFeatures,skipinitialspace=True)
 print(missing_values_table(tmp[trainFeatures]))
 del tmp
 
 print("Reading -> 'WqqWlv_Sh221_ade.csv'")
-tmp = pd.read_csv('data/WqqWlv_Sh221_ade.csv',nrows = int(nrows_WqqWlv*fraction))
+tmp = pd.read_csv('data/WqqWlv_Sh221_ade.csv',nrows = int(nrows_WqqWlv*fraction),usecols=trainFeatures,skipinitialspace=True)
 print(missing_values_table(tmp[trainFeatures]))
 del tmp
 
 print("Reading -> 'WJets_Sh221.csv'")
-tmp = pd.read_csv('data/WJets_Sh221.csv',nrows = int(fraction*nrows_Wjets))
+tmp = pd.read_csv('data/WJets_Sh221.csv',nrows = int(fraction*nrows_Wjets),usecols=trainFeatures,skipinitialspace=True)
 print(missing_values_table(tmp[trainFeatures]))
 del tmp
-'''
 print "Reading time: ", (time.time() - start)
