730425c9bd6ed732e0bb1a655fafd2ef4c375887
Dmytro Ostapchuk <dmytro.ostapchuk@ua.pt>
2019-08-05 11:12:00 +0100

    some changes on pc

--- modelWH.py
+++ modelWH.py
@@ -115,7 +115,7 @@ if __name__ == "__main__":
 
     # Fitting the Model -> TRAINING
     # fit(x=None, y=None, batch_size=None, epochs=1, verbose=1, callbacks=None, validation_split=0.0, validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0, steps_per_epoch=None, validation_steps=None, validation_freq=1)
-    history = model.fit(XDev, YDev, validation_data=(XVal,YVal,weightVal),sample_weight=weightDev,shuffle=True, **trainParams)
+    history = model.fit(XDev, YDev, validation_data=(XVal,YVal,weightVal),shuffle=True, **trainParams)
 
     acc = history.history['acc']
     val_acc = history.history['val_acc']
