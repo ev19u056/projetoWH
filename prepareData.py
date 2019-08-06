bc8f851806feb22cd1d4b32ee11d3ea379a940af
Dmytro Ostapchuk <dmytro.ostapchuk@ua.pt>
2019-08-06 17:54:19 +0100

    prepareData.py changes

--- prepareData.py
+++ prepareData.py
@@ -22,6 +22,7 @@ trainFeatures = ['nFats', 'nJets', 'nTags', 'nTaus', 'nMuons', 'nbJets', 'FJ1nTa
 usecols = trainFeatures[:]; usecols.append("EventWeight")
 scalingFeatures = ['mB1', 'mB2', 'mBB', 'mJ3', 'mL1', 'mTW', 'mVH', 'met', 'pTW', 'FJ1M', 'dRBB', 'mBBJ', 'mVFJ', 'pTB1', 'pTB2', 'pTBB', 'pTJ3', 'ptL1', 'FJ1C2', 'FJ1D2', 'FJ1Pt', 'etaB1', 'etaB2', 'etaBB', 'etaJ3', 'etaL1', 'pTBBJ', 'FJ1Ang', 'FJ1Eta', 'FJ1Phi', 'FJ1T21', 'dEtaBB', 'dPhiBB', 'metSig', 'FJ1KtDR', 'dPhiVBB', 'dPhiVFJ', 'MV2c10B1', 'MV2c10B2', 'metSig_PU', 'mindPhilepB', 'metOverSqrtHT', 'metOverSqrtSumET']
 
+luminosity = 135900
 nrows_signal = 991141
 nrows_stopWt = 277816
 nrows_ttbar = 4168037
