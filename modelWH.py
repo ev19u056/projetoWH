1210e3fc7eed12c3501dd528c78c40e936f4fa90
Dmytro Ostapchuk <dmytro.ostapchuk@ua.pt>
2019-08-14 14:43:18 +0100

    lr

--- modelWH.py
+++ modelWH.py
@@ -13,6 +13,7 @@ from keras.models import Sequential
 from keras.layers import Dense#, Dropout, AlphaDropout
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.callbacks import Callback
+from keras import backend
 
 ''' copy to your folder and adapt commonFunctions.py'''
 from commonFunctions import assure_path_exists, plotter, getYields, FullFOM#, myClassifier, gridClassifier, getDefinedClassifier,
