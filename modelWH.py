0c91577da6471cf3853eb075bb949a7647ed640c
Dmytro Ostapchuk <dmytro.ostapchuk@ua.pt>
2019-08-16 15:19:35 +0100

    modelWH

--- modelWH.py
+++ modelWH.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--dropoutRate', type=float, default=0, help='Drop-out rate')
     parser.add_argument('-r', '--regularizer', type=float, default=0, help='Regularizer')
     parser.add_argument('-i', '--iteration', type=int, default=1, help='Iteration number i')
-	parser.add_argument('-f', '--fraction', type=float, default=0, help='The fraction of available data to be loaded')
+	#parser.add_argument('-f', '--fraction', type=float, default=0, help='The fraction of available data to be loaded')
     parser.add_argument('-l', '--List', type=str, required=True, help='Defines the architecture of the NN; e.g: -l "14 12 7" -> 3 hidden layers of 14, 12 and 7 neurons respectively (input always 53, output always 1)')
     parser.add_argument('-act', '--act', type=str, default="relu", help='activation function for the hidden neurons')
     parser.add_argument('-ini', '--initializer', type=str, default="he_normal", help='Kernel Initializer for hidden layers')
